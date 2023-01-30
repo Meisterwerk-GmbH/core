41dda3ecd02d1f2095d3834843d79798f13bf414
Timo Bühlmann <timo.buehlmann@gmail.com>
2023-01-30 10:55:46 +0100

    exception-handler: add throw-on-param

--- src/ExceptionHandler.php
+++ src/ExceptionHandler.php
@@ -6,14 +6,21 @@ use Exception;
 
 class ExceptionHandler
 {
-    public static function handleUnexpectedError(\Exception|\Error $e, string $scope, $mailingCallback): void
+    public static function handleUnexpectedError(
+        \Exception|\Error $e,
+        string $scope,
+        $mailingCallback,
+        bool $throwOn = true
+    ): void
     {
         try {
             $mailingCallback($scope);
         } catch (\Exception|\Error) {
             throw new \Exception('Unexpected error and failed error handling (' . $scope . ')', 0, $e);
         }
-        throw new \Exception('Unexpected error (' . $scope . ')', 0, $e);
+        if ($throwOn) {
+            throw new \Exception('Unexpected error (' . $scope . ')', 0, $e);
+        }
     }
 
 }
